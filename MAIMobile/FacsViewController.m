d7ad3786689dfed0937e20d2c6c3c5399977ca64
Ilya Tsarev <idreyk@gmail.com>
2015-01-18 19:33:22 +0300

    New structure

---
+++ MAIMobile/FacsViewController.m
@@ -0,0 +1,37 @@
+//
+//  FacsViewController.m
+//  MAIMobile
+//
+//  Created by Ilya Tsarev on 18.01.15.
+//  Copyright (c) 2015 MAI. All rights reserved.
+//
+
+#import "FacsViewController.h"
+
+@interface FacsViewController ()
+
+@end
+
+@implementation FacsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ MAIMobile/NewsViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsViewController.h
+//  MAIMobile
+//
+//  Created by Ilya Tsarev on 18.01.15.
+//  Copyright (c) 2015 MAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsViewController : UIViewController
+
+@end
