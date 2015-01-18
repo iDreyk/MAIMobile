d7b653f37bbdce82129dc52531f97e777ec21973
AlexLev <alex-lev2312@mail.ru>
2015-01-18 17:39:04 +0300

    add two controllers to tap bar

---
+++ MAIMobile/OtherViewController.h
@@ -0,0 +1,13 @@
+//
+//  OtherViewController.h
+//  MAIMobile
+//
+//  Created by Алексей Левинтов on 18.01.15.
+//  Copyright (c) 2015 MAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherViewController : UIViewController
+
+@end
